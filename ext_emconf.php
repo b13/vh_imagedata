473b196a937a20598a9198b551fe4f16e88b21cd
David Steeb <david.steeb@b13.de>
2021-02-03 12:37:50 +0100

    [TASK] Set version number to 1.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'ViewHelper for accessing image data without rendering an image tag',
 	'description' => 'ViewHelper for accessing data of a (processed) image for further use (like img-tag w/ lazy loading etc.)',
 	'category' => 'fe',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'state' => 'stable',
 	'author' => 'David Steeb',
 	'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH, Stuttgart',
-);
+];
